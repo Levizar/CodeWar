b76eec9420a3f8fc68a68fb7b19433bf9f4fcecb
Brice Levizar Bartoletti <bartolettibrice@gmail.com>
2019-10-09 09:58:13 +0200

    end

---
+++ Write Number in Expanded Form.js
@@ -0,0 +1,18 @@
+function expandedForm(num) {
+    let len = num.toString().length;
+    let div = "";
+    let divmat=[];
+    let i = 0;
+    while(num>0){
+      div = "1" + "0".repeat(len-1);
+      div = parseInt(div);
+      divmat[i] = (Math.trunc(num/div))*div;
+      num = num - divmat[i];
+      len = len - 1;
+      i++;
+      }
+    let answer = [];
+    answer = divmat.filter( x => parseInt(x) > 0)
+    answer = answer.join(' + ')
+    return answer;
+  }
