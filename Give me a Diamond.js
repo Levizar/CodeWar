06b6100cf492a475ccacfd1bad9f73117b5b5c35
Brice Levizar Bartoletti <bartolettibrice@gmail.com>
2019-12-20 16:09:01 +0100

    maj

--- Give me a Diamond.js
+++ Give me a Diamond.js
@@ -1,22 +1,34 @@
 const diamond = (n) => {
     if (n % 2 == 0 || n <= 0) {
         return null
+    } else if (n == 1) {
+        return "*\n"
     } else {
         let array = [];
-        const limit = Math.floor(n / 2);
+        const limit = Math.floor((n / 2));
         for (i = 0; i < limit; i++) {
             let string = "";
             for (j = 0; j < limit; j++) {
-                (j < limit - i) ? string.concat(" "): string.concat("*");
+                (j < (limit - i)) ? (string = string.concat(" ")) : (string = string.concat("*"));
             }
-            let stringTrimed = string.trim();
-            stringTrimed.concat("\n");
-            array[i] = string + "*" + stringReversedTrimed;
+            let stringTrimed = string
+            stringTrimed = stringTrimed.trim();
+            stringTrimed = stringTrimed.concat("\n");
+            array[i] = string + "*" + stringTrimed;
         }
         arrayBot = array.reverse();
-        array.push(("*".repeat(n)));
+        array.push((("*".repeat(n)).concat("\n")));
         array = [...array, ...arrayBot];
         const XD = array.join('');
         return XD
     }
 }
+
+à obtenir
+    ' *\n***\n *\n'
+obtenu
+    ' *\n***\n *\n***\n'
+à obtenir
+    '  *\n ***\n*****\n ***\n  *\n'
+obtenu
+    ' ***\n  *\n*****\n ***\n  *\n*****\n'
