2e932b9cbcd13aaf1b406fa02be298769afd1069
Brice Levizar Bartoletti <bartolettibrice@gmail.com>
2019-12-20 15:21:34 +0100

    give me a diamond

---
+++ Give me a Diamond.js
@@ -0,0 +1,22 @@
+function diamond(n) {
+    if (n % 2 == 0 || n <= 0) {
+        return null
+    } else {
+        const array = [];
+        const limit = Math.floor(n / 2);
+        for (i = 0; i < limit; i++) {
+            let string = "";
+            for (j = 0; j < limit; j++) {
+                (j < limit - i) ? string.concat(" "): string.concat("*");
+            }
+            let stringTrimed = string.trim();
+            stringTrimed.concat("\n");
+            array[i] = string + "*" + stringReversedTrimed;
+        }
+        arrayBot = array.reverse();
+        array.push(("*".repeat(n)));
+        array = [...array, ...arrayBot];
+        const XD = array.join('');
+        return XD
+    }
+}
