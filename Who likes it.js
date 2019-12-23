a8477fe4ccf8785a063c228855fee375e836912b
Brice Bartoletti <bartolettibrice@gmail.com>
2019-12-23 23:33:03 +0100

    Who likes it

---
+++ Who likes it.js
@@ -0,0 +1,18 @@
+function likes(names) {
+    let namesLength = names.length;
+    if(!namesLength){
+        return "no one likes this"
+    } else if(namesLength < 2){
+        return `${names[0]} likes this`
+    } 
+    else if(namesLength < 4){
+        const last = names.pop();
+        names = names.join(', ');
+        return `${names} and ${last} like this`
+    } else{
+        let firstTwo = names.splice(0,2);
+        firstTwo = firstTwo.join(', ');
+        namesLength = names.length;
+        return `${firstTwo} and ${namesLength} others like this`
+    }
+}
