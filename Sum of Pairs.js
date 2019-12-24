49022e8b5e2293e762cb24b6b09453e2541ea905
Brice Bartoletti <bartolettibrice@gmail.com>
2019-12-24 12:31:28 +0100

    ok but timeout

--- Sum of Pairs.js
+++ Sum of Pairs.js
@@ -8,10 +8,10 @@ const sum_pairs = (ints, s) => {
     
     // 2nd test : test général en sautant les proches (j démarre à j+2 au lieu de +1)
     const sol =[];
-    for(let i = 0; i < ints.length - 2; i++){
-        for(let j = i + 2; j< ints.length - 1; j++){
+    for(let i = 0; i < ints.length - 1; i++){
+        for(let j = i + 2; j< ints.length; j++){
             if(ints[i] + ints[j] === s){
-                sol.push([ints[i], ints[j], j-1]);
+                sol.push([ints[i], ints[j], j-i]);
             }
         }
     }
