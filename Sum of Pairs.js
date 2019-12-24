162ea4e5fa473a239c4804768100a843c82bc37e
Brice Bartoletti <bartolettibrice@gmail.com>
2019-12-24 14:27:24 +0100

    sum of pairs

--- Sum of Pairs.js
+++ Sum of Pairs.js
@@ -1,3 +1,5 @@
+
+// This code timeout
 const sum_pairs = (ints, s) => {
     // 1er test: les proches
     for (let i = 0; i < (ints.length - 1); i++) {
@@ -15,7 +17,21 @@ const sum_pairs = (ints, s) => {
             }
         }
     }
+    
     const answer = sol.reduce((acc, elm, i, arr) => (elm[2] < acc[2]) ? elm : acc, sol[0])
     if(answer) answer.pop();
     return answer
+}
+
+// Autre solution: 1 boucle réel, une recherche sur set (= boucle + courte)
+
+const sum_pairs = (ints, s) => {
+    if (ints.length < 2) return undefined;
+    const firstPossiblePair = new Set();
+    firstPossiblePair.add(ints[0]);
+    for (let i = 1; i < ints.length; i++){
+        if(firstPossiblePair.has(s - ints[i])) return [(s - ints[i]), ints[i]];
+        firstPossiblePair.add(ints[i]);
+    }
+    return undefined
 }
