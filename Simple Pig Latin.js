2c0f29500ed37210d69a8c6686809c9b1c630348
Brice Bartoletti <bartolettibrice@gmail.com>
2019-12-25 22:58:48 +0100

    simple pig Latin

--- Simple Pig Latin.js
+++ Simple Pig Latin.js
@@ -1,3 +1,13 @@
 const pigIt = str => {
-    const arr = str.match(/([\w]+)/g);
+    const newStr = str.replace(/([\w]+)/g, word => {
+        let answer = [...word];
+        const toPush = answer.shift();
+        answer.push(toPush, "ay");
+        return answer.join("");
+    });
+    return newStr;
 };
+
+// refactor
+
+const pigIt = str => str.replace(/([\w]+)/g, word => word.slice(1) + word[0] + "ay");
