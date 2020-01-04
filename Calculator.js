4c2d5bf6b287e78396dd8a38add90be9ae88c276
Brice Levizar Bartoletti <bartolettibrice@gmail.com>
2020-01-04 14:45:44 +0100

    basic operation ok

--- Calculator.js
+++ Calculator.js
@@ -42,41 +42,81 @@ const Calculator = function () {
 
         }
 
-        // Multiplication
-        for (let i = 0; i < initArr.length; i++) {
-            if (initArr[i] === "*") {
-                let isSearchingNumber = true;
-                let j = i - 1;
-                let nbr1, nbr2;
-                let result, startIndexForSplice
-                while (isSearchingNumber) {
-                    if (/[\d]/.match(initArr[j])) {
-                        j -= 1;
-                    } else {
-                        isSearchingNumber = false;
-                        nbr1 = initArr.splice((j + 1), (i - j - 1), 0);
+        // function to perform one of the 4 basic operation
+        function basicOperation(operationToPerform) {
+            for (let i = 0; i < initArr.length; i++) {
+                if (initArr[i] === operationToPerform) {
+                    let isSearchingNumber = true;
+                    let j = i - 1;
+                    let nbr1, nbr2;
+                    let result, startIndexForSplice
+                    while (isSearchingNumber) {
+                        if (/[\d]/.test(initArr[j])) {
+                            j -= 1;
+                        } else {
+                            isSearchingNumber = false;
+                            console.log("i : ", i, initArr);
+                            nbr1 = initArr.splice((j + 1), (i - j - 1));
+                            console.log("i : ", i, initArr);
+                        }
                     }
-                }
-                startIndexForSplice = j + 1;
-                isSearchingNumber = true;
-                j = i + 1;
-                while (isSearchingNumber) {
-                    if (/[\d]/.match(initArr[j])) {
-                        j += 1;
-                    } else {
-                        isSearchingNumber = false;
-                        nbr2 = initArr.splice((i + 1), (j - i - 1), 0);
-                        // reset the i to before the previous position
-                        i = j;
+                    startIndexForSplice = j + 1;
+                    isSearchingNumber = true;
+                    j = i + 1;
+                    while (isSearchingNumber) {
+                        if (/[\d]/.test(initArr[j])) {
+                            j += 1;
+                        } else {
+                            isSearchingNumber = false;
+                            nbr2 = initArr.splice((i), (j - i));
+                            // reset the i to before the previous position
+                            i = j - 2;
+                        }
                     }
+
+                    nbr1 = parseFloat(nbr1.join(''));
+                    nbr2 = parseFloat(nbr2.join(''));
+
+                    switch (operationToPerform) {
+                        case "*":
+                            result = +nbr1 * nbr2;
+                            break;
+                        case "/":
+                            result = +nbr1 / nbr2;
+                            break;
+                        case "+":
+                            result = +nbr1 + nbr2;
+                            break;
+                        case "-":
+                            result = +nbr1 - nbr2;
+                            break;
+                        default:
+                            break;
+                    }
+                    initArr.splice(startIndexForSplice, 1, result);
                 }
-                nbr1 = nbr1.join('');
-                nbr2 = nbr2.join('');
-                result = +nbr1 + nbr2
-                initArr.splice(startIndexForSplice, 0, result);
             }
         }
+        ["*", "/", "+", "-"].forEach(operation => basicOperation(operation));
 
         // To do: other operation: EMDAS
+
+        return initArr.join('');
     };
 };
+
+// test script:
+
+function test(stringTest) {
+    let calculate = new Calculator();
+    let b = calculate.evaluate(stringTest);
+    console.log("***********", stringTest, " = ", b);
+}
+
+test("22 * 6 + 22 + 22 + 22 + 22");
+test("22 * (10 / 10)");
+
+
+
+
+// P E M D A S 
