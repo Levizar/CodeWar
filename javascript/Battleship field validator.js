a44349b750309000c53464cb9cd6b8c9496a8b8a
Brice Bartoletti <bartolettibrice@gmail.com>
2020-01-25 22:10:54 +0100

    reorganization

---
+++ javascript/Battleship field validator.js
@@ -0,0 +1,8 @@
+const validateBattlefield = field => {
+    let isBattlefieldValid = true; // default value
+
+    // No ship touches
+    for (let i = 0; i < field.length; i++) {
+        for (let j = 0; j < field[i].length; j++) {}
+    }
+};
