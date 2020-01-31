f2233de1f634419d93899ef7557292d2ef6a74f3
Brice Levizar Bartoletti <bartolettibrice@gmail.com>
2020-01-31 22:46:24 +0100

    last digit of large number done

---
+++ javascript/Last digit of a large number.js
@@ -0,0 +1,13 @@
+const lastDigit = (str1, str2) => {
+    if (str2 === "0") return 1;
+    if (str1 === "0") return 0;
+    let rest = 0;
+    for (let i = 0; i < str2.length; i++) {
+        rest = (rest * 10 + parseInt((str2[i]), 10)) % 4;
+    }
+    if (rest === 0) rest = 4;
+    return Math.pow(str1.slice(-1), rest) % 10;
+}
+
+
+const lastDigit = (str1, str2) => +str2 === 0 ? 1 : Math.pow(+str1.slice(-1), ((+str2.slice(-2) % 4) + 4)) % 10;
