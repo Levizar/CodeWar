a6b1d1a3b5cbb5b45163ff23e888a7e07726a157
Brice Bartoletti <bartolettibrice@gmail.com>
2020-02-08 21:55:03 +0100

    help the general decode secret ennemy message.js

--- javascript/3 Kyu/Help the general decode secret enemy messages.js
+++ javascript/3 Kyu/Help the general decode secret enemy messages.js
@@ -1,16 +1,41 @@
-// device.encode ('')
+// // device.encode ('')
 
-let letter = "abcdefghijklmnopqrstuvwxyz";
-letter = letter.toUpperCase() + letter + "?,. "
+// let possibleLetter = "abcdefghijklmnopqrstuvwxyz";
+// possibleLetter = possibleLetter.toUpperCase() + possibleLetter + "?,. "
+// device.decode = sentence => {
+//     const dictionary = [];
+//     // Testing all position
+//     for (let i = 0; i < sentence.length; ++i) {
+//         const alphabetForThisPosition = {};
+//         for (let j = 0; j < possibleLetter.length; ++j) {
+//             const letter = possibleLetter[j];
+//         }
+//         dictionary.push(alphabetForThisPosition);
+//     }
+// }
+
+// Test 2
+
+let possibleLetter = "abcdefghijklmnopqrstuvwxyz";
+possibleLetter = possibleLetter.toUpperCase() + possibleLetter + "?,. ";
 device.decode = sentence => {
-    let dictionary = {};
-    for (let i = 0; i < letter.length; ++i) {
-        let toEncrypt = "";
-        for (let j = 0; j < sentence.length; ++j) {
-            toEncrypt += letter[i];
+    let positionDictionary = [...Array(possibleLetter.length)].map(x => ({}));
+    // For each letter => get the correspondance
+    for (let i = 0; i < possibleLetter.length; ++i) {
+        const letters = possibleLetter[i].repeat(sentence.length);
+        const cryptedLetters = device.encode(letters);
+        // For each correspondance => put it in the position relative dictionnary
+        for (let j = 0; j < cryptedLetters.length; ++j) {
+            const singleCryptedLetter = cryptedLetters[j];
+            positionDictionary[j][`${singleCryptedLetter}`] = possibleLetter[i];
         }
-        const encrypted = device.decode(toEncrypt);
-        
-
     }
-}
+    // Use the dictionnary to decode the sentence
+    let decodedSentence = "";
+    for (let i = 0; i < sentence.length; i++) {
+        const codedLetter = sentence[i];
+        const decodedLetter = positionDictionary[i][codedLetter];
+        decodedSentence += decodedLetter;
+    }
+    return decodedSentence;
+};
