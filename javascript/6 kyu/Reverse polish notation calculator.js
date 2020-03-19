3a39ca907d005eb9974370c0db87db096e8ce906
Brice Bartoletti <bartolettibrice@gmail.com>
2020-03-19 21:44:01 +0100

    Reverse polish notation calculator.js

---
+++ javascript/6 kyu/Reverse polish notation calculator.js
@@ -0,0 +1,19 @@
+const calc = (str) => {
+    const stack = [];
+    const arr =str.split(' ')
+    arr.forEach(elm => {
+        let result;
+        if(elm === "sqrt"){
+            const nb1 = stack.pop();
+            result = Math.sqrt(nb1);
+        } else if(isNaN(+elm)){
+            const nb2 = stack.pop();
+            const nb1 = stack.pop();
+            result = eval(`${nb1} ${elm} ${nb2}`);
+        }else{
+            result = elm;
+        }
+        stack.push(result)
+    });
+    return +stack[0];
+}
