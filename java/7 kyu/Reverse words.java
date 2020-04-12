8cd6e94a6abdbf71e9af7439ffb82faca1b77f16
Brice Bartoletti <bartolettibrice@gmail.com>
2020-04-12 14:44:25 +0200

    Java reverse word - second solution

---
+++ java/7 kyu/Reverse words.java
@@ -0,0 +1,36 @@
+/**
+ * @author: Brice
+ */
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class Kata{
+  public static String reverseWords(final String original){
+      String[] arrOfStringChar = original.split(" ");
+      if(arrOfStringChar.length == 0) return original;
+      return Arrays.stream(arrOfStringChar)
+        .map(w->{
+            String[] splitedWord = w.split("");
+            return IntStream
+                .rangeClosed(1, splitedWord.length)
+                .mapToObj(i -> (String) splitedWord[splitedWord.length - i])
+                .map(Object::toString)
+                .reduce("",String::concat);
+        })
+        .reduce("", (acc, item) -> acc.equals("") ? item : (acc + " " + item));
+  }
+}
+
+// Other solution inspired by others' answer:
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class Kata{
+  public static String reverseWords(final String original){
+      return Arrays.stream(original.split("(?<=\s)|(?=\s+)"))
+        .map(word -> (new StringBuilder(word)).reverse().toString())
+        .collect(Collectors::joining);
+  }
+}
