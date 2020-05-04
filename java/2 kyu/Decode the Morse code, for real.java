cc775b668c27e58e4da012ed94bcc09a5c18e66b
Brice Bartoletti <bartolettibrice@gmail.com>
2020-05-04 22:47:17 +0200

    decode morse K-means nearly finished

--- java/2 kyu/Decode the Morse code, for real.java
+++ java/2 kyu/Decode the Morse code, for real.java
@@ -1,3 +1,5 @@
+/*
+
 import java.util.Arrays;
 import java.util.stream.Collectors;
 
@@ -43,7 +45,7 @@ public class MorseCodeDecoder {
         
         // dash-dot/2  => before limit = dot after limit = dash
         final double limitValue1 = (max1 - min1) / 2.0;
-final double limitValue2 = (arrOf0.length == 1 && arrOf0[0].length() < 6) ? max0 : (limitValue1 + ((max0 - limitValue1) / 2.0));
+        final double limitValue2 = (arrOf0.length == 1 && arrOf0[0].length() < 6) ? max0 : (limitValue1 + ((max0 - limitValue1) / 2.0));
 
         int dotTopLine = (int) Math.floor(limitValue1);
         dotTopLine = dotTopLine == 0 ? min1 : dotTopLine;
@@ -94,5 +96,145 @@ final double limitValue2 = (arrOf0.length == 1 && arrOf0[0].length() < 6) ? max0
                     
         System.out.println(store);
         return store;
+    }
+}
+*/
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import static java.lang.Math.abs;
+
+public class MorseCodeDecoder {
+    
+    public static String decodeMorse(final String morseCode) {
+        if(morseCode.equals(" ")|morseCode.equals("")) return "";
+        String store = Arrays.stream(morseCode.trim().split("   "))
+                .map(word -> Arrays.stream(word.split(" "))
+                                .map(MorseCode::get)
+                                .collect(Collectors.joining("")))
+                .collect(Collectors.joining(" "))
+                .trim();
+        System.out.println(store);
+        return store;
+    }
+    
+    public static String decodeBitsAdvanced(String bits) {
+        // Clean extra 0
+        final String cleanedBits = bits.replaceAll("^0+|0+$|^[ ]|[ ]$", "");
+        if(cleanedBits.equals(" ")|cleanedBits.equals("")) return "";
+        System.out.println(cleanedBits);        
+        
+        // Create array which we can work with
+        final String[] arrOfBits = cleanedBits.split("(?<=0)(?=1)|(?<=1)(?=0)");
+        final Integer[] arrOfBitsLength = Arrays.stream(arrOfBits).mapToInt(String::length).boxed().toArray(size -> new Integer[size]);
+
+        // Initialize 3 ArrayList for K-means alg
+        List<Integer> cluster1;
+        List<Integer> cluster2;
+        List<Integer> cluster3;
+        
+        double cluster1PreCentroid;
+        double cluster2PreCentroid;
+        double cluster3PreCentroid;
+        double cluster1NewCentroid = (double) Arrays.stream(arrOfBitsLength).min(Integer::compare).get();
+        double cluster3NewCentroid = (double) Arrays.stream(arrOfBitsLength).max(Integer::compare).get();
+        double cluster2NewCentroid = (cluster3NewCentroid - cluster1NewCentroid) / 2.0;
+        
+        do{
+        
+            cluster1PreCentroid = cluster1NewCentroid;
+            cluster2PreCentroid = cluster2NewCentroid;
+            cluster3PreCentroid = cluster3NewCentroid;
+           
+            System.out.println("cluster1 :" + cluster1.toString());
+            System.out.println("cluster2 :" + cluster2.toString());
+            System.out.println("cluster3 :" + cluster3.toString());
+            
+            cluster1 = new ArrayList<Integer>();
+            cluster2 = new ArrayList<Integer>();
+            cluster3 = new ArrayList<Integer>();
+            
+            System.out.println("cluster1PreCentroid : " + cluster1PreCentroid);
+            System.out.println("cluster2PreCentroid : " + cluster2PreCentroid);
+            System.out.println("cluster3PreCentroid : " + cluster3PreCentroid);
+
+            for (int i = 0; i < arrOfBitsLength.length; i++) {
+                Integer elm = arrOfBitsLength[i];
+                double distanceToCluster1 = abs(elm - cluster1PreCentroid);
+                double distanceToCluster2 = abs(elm - cluster2PreCentroid);
+                double distanceToCluster3 = abs(elm - cluster3PreCentroid);
+
+                if(distanceToCluster1 <= distanceToCluster2 && distanceToCluster1 <= distanceToCluster3){
+                    cluster1.add(elm);
+                } else if(distanceToCluster2 <= distanceToCluster1 && distanceToCluster2 <= distanceToCluster3){
+                    cluster2.add(elm);
+                } else if(distanceToCluster3 <= distanceToCluster1 && distanceToCluster3 <= distanceToCluster2){
+                    cluster3.add(elm);
+                } else {
+                    System.out.println("4th case shouldn't happen");
+                }
+            }
+            
+            cluster1NewCentroid = cluster1.stream().mapToDouble(val -> val).average().orElse(0.0);
+            cluster2NewCentroid = cluster2.stream().mapToDouble(val -> val).average().orElse(0.0);
+            cluster3NewCentroid = cluster3.stream().mapToDouble(val -> val).average().orElse(0.0);
+
+        } while (cluster1PreCentroid != cluster1NewCentroid && cluster2PreCentroid != cluster2NewCentroid && cluster3PreCentroid != cluster3NewCentroid);
+
+        System.out.println("bits:" + bits);
+        System.out.println("cleanedBits:" + cleanedBits);
+        System.out.println("arrOfBitsLength :" + Arrays.toString(arrOfBitsLength));
+        System.out.println("cluster1 :" + cluster1.toString());
+        System.out.println("cluster2 :" + cluster2.toString());
+        System.out.println("cluster3 :" + cluster3.toString());
+        System.out.println(cluster1NewCentroid);
+        System.out.println(cluster2NewCentroid);
+        System.out.println(cluster3NewCentroid);
+
+        StringBuilder store = new StringBuilder();
+
+        for (int i = 0; i < arrOfBitsLength.length; i++) {
+            Integer elm = arrOfBitsLength[i];
+            double distanceToCluster1 = abs(elm - cluster1NewCentroid);
+            double distanceToCluster2 = abs(elm - cluster2NewCentroid);
+            double distanceToCluster3 = abs(elm - cluster3NewCentroid);
+
+            if(distanceToCluster1 <= distanceToCluster2 && distanceToCluster1 <= distanceToCluster3){
+                // if match 0 => remplacer par espace
+                // if match 1 => remplacer par dot
+            } else if(distanceToCluster2 <= distanceToCluster1 && distanceToCluster2 <= distanceToCluster3){
+                // if match 0 => remplacer par 3 espace
+                // if match 1 => remplacer par dash
+            } else if(distanceToCluster3 <= distanceToCluster1 && distanceToCluster3 <= distanceToCluster2){
+                // if match 0 => remplacer par 7 espace
+                // if match 1 => remplacer par dash
+            } else {
+                System.out.println("4th case shouldn't happen");
+            }
+        }
+
+        /*
+        final String dotRegex = String.format("1{%s,%s}", 1, dotTopLine);
+        final String inCharSepRegex = String.format("0{%s,%s}", 1, dotTopLine);
+        final String dashRegex = String.format("1{%s,}", dashBottomLine);
+        final String outCharSepRegex = String.format("0{%s,%s}", dashBottomLine, outCharTopLine);
+        final String wordSepRegex = String.format("0{%s,}", wordBottomLine);
+
+        // 110110100111000001100000011111101001111100111111000000000001110111111110111110111110000001011000111111000001111100111011000001
+        // ···· · −·−−   ·−−− ··− −·· ·
+        // Start the conversion
+        String store = cleanedBits.replaceAll(dashRegex, "-")
+                    .replaceAll(dotRegex, ".")
+                    .replaceAll(wordSepRegex, "   ")
+                    .replaceAll(outCharSepRegex, " ")
+                    .replaceAll(inCharSepRegex, "");
+                    
+        System.out.println(store);
+        return store;
+        */
+        return "";
     }
 }
