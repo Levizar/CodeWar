fabfe2b3ceb5dbe8f401e8a92f8f4a32337594ed
Brice Bartoletti <bartolettibrice@gmail.com>
2020-05-09 23:39:53 +0200

    java 2kyu decode morse code OOP OK !

--- java/2 kyu/Decode the Morse code, for real.java
+++ java/2 kyu/Decode the Morse code, for real.java
@@ -1,3 +1,143 @@
+// OOP Attempt:
+
+// Thx to:
+// https://www.baeldung.com/java-k-means-clustering-algorithm
+// https://www.analyticsvidhya.com/blog/2016/11/an-introduction-to-clustering-and-different-methods-of-clustering/
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import static java.lang.Math.abs;
+
+public class MorseCodeDecoder {
+    
+    public static String decodeMorse(final String morseCode) {
+        if(morseCode.equals(" ")|morseCode.equals("")) return "";
+        String store = Arrays.stream(morseCode.trim().split("   "))
+                .map(word -> Arrays.stream(word.split(" "))
+                                .map(MorseCode::get)
+                                .collect(Collectors.joining("")))
+                .collect(Collectors.joining(" "))
+                .trim();
+        return store;
+    }
+    
+    
+    public static String decodeBitsAdvanced(String bits) {
+        // Clean extra 0
+        final String cleanedBits = bits.replaceAll("^0+|0+$|^[ ]|[ ]$", "");
+        if(cleanedBits.equals(" ")|cleanedBits.equals("")) return "";
+
+        // Create array which we can work with
+        final String[] arrOfBits = cleanedBits.split("(?<=0)(?=1)|(?<=1)(?=0)");
+        final Integer[] arrOfBitsLength = Arrays.stream(arrOfBits).mapToInt(String::length).boxed().toArray(size -> new Integer[size]);
+        
+        ClusterMachine137 clusterMachine = new ClusterMachine137();
+        clusterMachine.run(arrOfBitsLength);
+
+        double dotCenter = clusterMachine.getCluster1Centroid();
+        double dashCenter = clusterMachine.getCluster2Centroid() != 0 ? clusterMachine.getCluster2Centroid() : dotCenter + 1;
+        double spaceCenter = clusterMachine.getCluster3Centroid() > 5 ? clusterMachine.getCluster3Centroid() : 7 ;
+        
+        StringBuilder store = new StringBuilder();
+        String regex1 = "1+";
+        String regex0 = "0+";
+        
+        for (int i = 0; i < arrOfBitsLength.length; i++) {
+            Integer length = arrOfBitsLength[i];
+            double distanceToCluster1 = abs(length - dotCenter);
+            double distanceToCluster2 = abs(length - dashCenter);
+            double distanceToCluster3 = abs(length - spaceCenter);
+            
+            String elmToTranslate = arrOfBits[i];
+            
+            
+            if(distanceToCluster1 <= distanceToCluster2 && distanceToCluster1 <= distanceToCluster3){
+                if(elmToTranslate.matches(regex0)) store.append("");
+                if(elmToTranslate.matches(regex1)) store.append(".");
+            } else if(distanceToCluster2 <= distanceToCluster1 && distanceToCluster2 <= distanceToCluster3){
+                if(elmToTranslate.matches(regex0)) store.append(" ");
+                if(elmToTranslate.matches(regex1)) store.append("-");
+            } else if(distanceToCluster3 <= distanceToCluster1 && distanceToCluster3 <= distanceToCluster2){
+                if(elmToTranslate.matches(regex0)) store.append("   ");
+                if(elmToTranslate.matches(regex1)) store.append("-");
+            } else {
+                System.out.println("4th case shouldn't happen");
+            }
+        }
+        return store.toString();
+    }
+
+    public static class ClusterMachine137{
+        // Initialize variables for K-means alg
+        private List<Integer> cluster1 = new ArrayList<Integer>();
+        private List<Integer> cluster2 = new ArrayList<Integer>();
+        private List<Integer> cluster3 = new ArrayList<Integer>();
+        
+        private double cluster1NewCentroid;
+        private double cluster2NewCentroid;
+        private double cluster3NewCentroid;
+        
+        private void run(Integer[] arrOfBitsLength){
+
+            double cluster1PreCentroid;
+            double cluster2PreCentroid;
+            double cluster3PreCentroid;
+            // alter the initiales values to get to he desired optimimum
+            cluster1NewCentroid = (double) Arrays.stream(arrOfBitsLength).min(Integer::compare).get();
+            cluster3NewCentroid = (double) Arrays.stream(arrOfBitsLength).max(Integer::compare).get();
+            cluster2NewCentroid = (cluster3NewCentroid - cluster1NewCentroid) / 2.0;
+    
+            do{
+            
+                cluster1PreCentroid = cluster1NewCentroid;
+                cluster2PreCentroid = cluster2NewCentroid;
+                cluster3PreCentroid = cluster3NewCentroid;
+               
+                cluster1 = new ArrayList<Integer>();
+                cluster2 = new ArrayList<Integer>();
+                cluster3 = new ArrayList<Integer>();
+                
+                for (int i = 0; i < arrOfBitsLength.length; i++) {
+                    Integer elm = arrOfBitsLength[i];
+                    double distanceToCluster1 = abs(elm - cluster1PreCentroid);
+                    double distanceToCluster2 = abs(elm - cluster2PreCentroid);
+                    double distanceToCluster3 = abs(elm - cluster3PreCentroid);
+    
+                    if(distanceToCluster1 <= distanceToCluster2 && distanceToCluster1 <= distanceToCluster3){
+                        cluster1.add(elm);
+                    } else if(distanceToCluster2 <= distanceToCluster1 && distanceToCluster2 <= distanceToCluster3){
+                        cluster2.add(elm);
+                    } else if(distanceToCluster3 <= distanceToCluster1 && distanceToCluster3 <= distanceToCluster2){
+                        cluster3.add(elm);
+                    } else {
+                        System.out.println("4th case shouldn't happen");
+                    }
+                }
+                
+                cluster1NewCentroid = Math.ceil(cluster1.stream().mapToDouble(val -> val).average().orElse(0.0));
+                cluster2NewCentroid = Math.ceil(cluster2.stream().mapToDouble(val -> val).average().orElse(0.0));
+                cluster3NewCentroid = Math.ceil(cluster3.stream().mapToDouble(val -> val).average().orElse(0.0));
+    
+            } while (cluster1PreCentroid != cluster1NewCentroid && cluster2PreCentroid != cluster2NewCentroid && cluster3PreCentroid != cluster3NewCentroid);
+        }
+
+        public double getCluster1Centroid(){
+            return /*cluster1NewCentroid <= 1 ? 1 : */cluster1NewCentroid;
+        }
+        public double getCluster2Centroid(){
+            return /*cluster2NewCentroid <= 3 ? 3 : */cluster2NewCentroid;
+        }
+        public double getCluster3Centroid(){
+            return /*cluster3NewCentroid <= 7 ? 7 : */cluster3NewCentroid;
+        }
+
+    }
+}
+
+/*
+
 // Seperate K-means for 1 and 0
 
 // Thx to:
@@ -131,6 +271,9 @@ public class MorseCodeDecoder {
         return store.toString();
     }
 }
+
+*/
+
 
 // // Thx to:
 // // https://www.baeldung.com/java-k-means-clustering-algorithm
