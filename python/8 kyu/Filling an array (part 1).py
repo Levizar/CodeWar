d4760980eb68d7a0df706c2805eb4d0f10bfc59f
Brice Bartoletti <bartolettibrice@gmail.com>
2020-09-01 13:16:27 +0200

    Python - 8kyu - Filling an array (part 1)

---
+++ python/8 kyu/Filling an array (part 1).py
@@ -0,0 +1,11 @@
+
+
+# Refactor with others's solution
+def arr(n=0): 
+    return list(range(n))
+
+# original code
+# def arr( n = 0 ):
+#     array = []
+#     for i in range( n ): array.append(i)
+#     return array
