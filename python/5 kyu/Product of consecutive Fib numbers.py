ad07c0c06cae6fbadc3c5d52c93b59b6918f684a
Brice Bartoletti <bartolettibrice@gmail.com>
2020-09-03 09:51:29 +0200

    Python - Product of consecutive Fib numbers.py - Refactor

--- python/5 kyu/Product of consecutive Fib numbers.py
+++ python/5 kyu/Product of consecutive Fib numbers.py
@@ -1,10 +1,16 @@
+# Refactor inspired by other's solution using Python's features
 def productFib(prod):
-    # your code
-    fib1 = fib_next = 0
-    fib2 = 1
-    found = False
+    fib1, fib2 = 0, 1
     while fib1 * fib2 < prod:
-        fib1 = fib2
-        fib2 = fib_next
-        fib_next = fib1 + fib2
+        fib1, fib2 = fib2, fib1 + fib2
     return [fib1, fib2, fib1 * fib2 == prod]
+
+# # Original solution
+# def productFib(prod):
+#     fib1 = fib_next = 0
+#     fib2 = 1
+#     while fib1 * fib2 < prod:
+#         fib1 = fib2
+#         fib2 = fib_next
+#         fib_next = fib1 + fib2
+#     return [fib1, fib2, fib1 * fib2 == prod]
