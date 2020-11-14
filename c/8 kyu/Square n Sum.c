ead5c8e58847d7f0ec03fe11c5fafbc5a6014fa9
Brice Bartoletti <bartolettibrice@gmail.com>
2020-11-14 08:13:59 +0100

    format Square n Sum.c

--- c/8 kyu/Square n Sum.c
+++ c/8 kyu/Square n Sum.c
@@ -3,7 +3,7 @@
 int square_sum(const int *values, size_t count)
 {
   int squaredSum = 0;
-  for(int i=0; i<count; ++i)
+  for (int i = 0; i < count; ++i)
   {
     squaredSum += *(values + i) * *(values + i);
   }
