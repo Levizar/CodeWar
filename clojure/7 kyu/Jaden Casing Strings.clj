deba9f04cdd1802f99cda9d49ad89924e1ef53a7
Brice Bartoletti <bartolettibrice@gmail.com>
2020-11-14 18:04:44 +0100

    Jaden Casing Strings.clj

---
+++ clojure/7 kyu/Jaden Casing Strings.clj
@@ -0,0 +1,39 @@
+;; second refactor
+(ns jaden-case 
+  (:require [clojure.string :as string]))
+
+(defn jaden-case [str] 
+  (string/join " " 
+               (map string/capitalize 
+                    (string/split str #" ")
+                    )
+               )
+  )
+
+;; first refactor
+;;
+;; (ns jaden-case)
+;; (require '[clojure.string :as string])
+
+;; (defn jaden-case [str]
+;;   (string/join " "
+;;                        (map string/capitalize 
+;;                             (string/split str #" ")
+;;                             )
+;;                        )
+;;   )
+
+
+
+
+;; first solution
+;; 
+;; (ns jaden-case)
+
+;; (defn jaden-case [str]
+;;   (clojure.string/join " "
+;;                        (map clojure.string/capitalize 
+;;                             (clojure.string/split str #" ")
+;;                             )
+;;                        )
+;;   )
