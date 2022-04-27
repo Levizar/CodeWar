d03f0f34b837c325afb43424ccf3f387fec67425
Brice Bartoletti <bartolettibrice@gmail.com>
2022-04-27 08:56:48 +0200

    Evaluate mathematical expression - improvement

--- python/2 kyu/Evaluate mathematical expression.py
+++ python/2 kyu/Evaluate mathematical expression.py
@@ -4,8 +4,7 @@ import re
 def calc(expression):
     transitions = {
         'formatting_expression': ('reducing_parentheses', format_expression),
-        'reducing_parentheses': ('eliminate_minus_redundancy', eliminate_parentheses),
-        'eliminate_minus_redundancy': ('perform_multiplication', eliminate_minus_redundancy),
+        'reducing_parentheses': ('perform_multiplication', eliminate_parentheses),
         'perform_multiplication': ('perform_addition', perform_multiplication),
         'perform_addition': ('format_output', perform_addition),
         'format_output': ('done', format_output)
@@ -26,10 +25,20 @@ def eliminate_parentheses(expression):
     regex = re.compile(r"[(][^()]*[)]")
     while regex.search(expression):
         expression = regex.sub(lambda a: str(calc(a[0][1:-1])), expression)
-    return expression
+    return eliminate_sign_redundancy(expression)
 
 
-def eliminate_minus_redundancy(expression):
+def perform_multiplication(expression):
+    regex = re.compile(r"(?P<a>\d+\.*\d*)(?P<op>[*/])(?P<b>[-+]?\d+\.*\d*)")
+    return perform_substitution(regex, expression)
+
+
+def perform_addition(expression):
+    regex = re.compile(r"(?P<a>-?\d+\.*\d*)(?P<op>[+-])(?P<b>[-+]?\d+\.*\d*)")
+    return perform_substitution(regex, expression)
+
+
+def eliminate_sign_redundancy(expression):
     regex = re.compile(r"--|\+\+|-\+|\+-")
     corresp = {
         '--': '+',
@@ -42,16 +51,6 @@ def eliminate_minus_redundancy(expression):
     return expression
 
 
-def perform_multiplication(expression):
-    regex = re.compile(r"(?P<a>-?\d+\.*\d*)(?P<op>[*/])(?P<b>[-+]?\d+\.*\d*)")
-    return perform_substitution(regex, expression)
-
-
-def perform_addition(expression):
-    regex = re.compile(r"(?P<a>-?\d+\.*\d*)(?P<op>[+-])(?P<b>[-+]?\d+\.*\d*)")
-    return perform_substitution(regex, expression)
-
-
 def perform_substitution(regex, expression):
     operations = {
         '*': lambda a, b: a * b,
@@ -59,9 +58,10 @@ def perform_substitution(regex, expression):
         '+': lambda a, b: a + b,
         '-': lambda a, b: a - b,
     }
+    # only perform 1 substitution at a time to avoid group intersection
     while regex.search(expression):
-        expression = regex.sub(lambda m: str(operations[m['op']](float(m['a']), float(m['b']))), expression)
-    return expression
+        expression = regex.sub(lambda m: str(operations[m['op']](float(m['a']), float(m['b']))), expression, 1)
+    return eliminate_sign_redundancy(expression)
 
 
 def format_output(expression):
@@ -79,21 +79,25 @@ def test(fun, inp, expected_output):
         print(f"output: {output} , expected: {expected_output} , input: {inp}")
 
 
-test(calc, '(1+1)', 2)
-test(calc, '(1+1)+(2+2)', 6)
-test(calc, '((1+1))', 2)
-test(calc, '1+1', 2)
-test(calc, '(-(-1))', 1)
-test(calc, '123', 123)
-test(calc, '-123', -123)
-test(calc, '12* 123', 1476)
-test(calc, '4/2', 2)
-test(calc, '2*2*2', 8)
-test(calc, '2 /2+3 * 4.75- -6', 21.25)
-test(calc, '2 / (2 + 3) * 4.33 - -6', 7.732)
-test(calc, '12*-1', -12)
-test(calc, "(1 - 2) + -(-(-(-4)))", 3)
-test(calc, "12 * -123", -1476)
-test(calc, "2 * -2 * 4", -16)
-test(calc, "12* 123/-(-5 + 2)", 492)
-test(calc, "(123.45*(678.90 / (-2.5+ 11.5)-(((80 -(19))) *33.25)) / 20) - (123.45*(678.90 / (-2.5+ 11.5)-(((80 -(19))) *33.25)) / 20) + (13 - 2)/ -(-11) ", 1)
+# test(calc, '(1+1)', 2)
+# test(calc, '(1+1)+(2+2)', 6)
+# test(calc, '((1+1))', 2)
+# test(calc, '1+1', 2)
+# test(calc, '(-(-1))', 1)
+# test(calc, '123', 123)
+# test(calc, '-123', -123)
+# test(calc, '12* 123', 1476)
+# test(calc, '4/2', 2)
+# test(calc, '2*2*2', 8)
+# test(calc, '2 /2+3 * 4.75- -6', 21.25)
+# test(calc, '2 / (2 + 3) * 4.33 - -6', 7.732)
+# test(calc, '12*-1', -12)
+# test(calc, "(1 - 2) + -(-(-(-4)))", 3)
+# test(calc, "12 * -123", -1476)
+# test(calc, "2 * -2 * 4", -16)
+# test(calc, "12* 123/-(-5 + 2)", 492)
+# test(calc, "(123.45*(678.90 / (-2.5+ 11.5)-(((80 -(19))) *33.25)) / 20) - (123.45*(678.90 / (-2.5+ 11.5)-(((80 -(19))) *33.25)) / 20) + (13 - 2)/ -(-11) ", 1)
+# test(calc, "-80 - ((((54 * 4) * -67) / -28) / -39) - (-34 * -5)", -236.74725274725273)
+# test(calc, "-97 + -13 + -74 - -100 - 87 - 5 / -10 + -46", -216.5)
+# test(calc, "-80 - 54 * 4 * -67 / -28 / -39 - -34 * -5", -236.74725274725273)
+test(calc, "-91 * 25 * 95 / -86 + 88 - 38 + -68 - -81", 2576.0813953488373)
